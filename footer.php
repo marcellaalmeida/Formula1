3a11d64975bfcc524c003528ef6fea6618c1c728
marcellaalmeida <152787532+marcellaalmeida@users.noreply.github.com>
2024-03-14 17:43:05 -0300

    Add files via upload
    Atualizações efetuadas na aula do dia 14 de março de 2024.

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div>
+
+<!-- Container que abriga o rodapé da página -->
+<div class="container-fluid text-center bg-dark text-light fixed-bottom">
+    <p>Sistema Web desenvolvido por Marcella</p>
+</div>
+
+
+
+</body>
+</html>
